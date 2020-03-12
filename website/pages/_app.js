192a999c795d0f7804dd52c1623a46879f407505
Jonathan Neal <jonathantneal@hotmail.com>
2020-03-12 19:49:29 -0400

    Use Head Component

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -2,7 +2,6 @@ import './style.css'
 import App from 'next/app'
 import NProgress from 'nprogress'
 import Router from 'next/router'
-import DefaultHeadTags from '../components/default-head-tags'
 import ProductSubnav from '../components/subnav'
 import MegaNav from '../components/mega-nav'
 import Footer from '@hashicorp/react-footer'
@@ -10,7 +9,8 @@ import { ConsentManager, open } from '@hashicorp/react-consent-manager'
 import consentManagerConfig from '../lib/consent-manager-config'
 import bugsnagClient from '../lib/bugsnag'
 import Error from './_error'
-import subnavLinks from '../data/subnav'
+import Head from 'next/head'
+import HashiHead from '@hashicorp/react-head'
 
 Router.events.on('routeChangeStart', NProgress.start)
 Router.events.on('routeChangeError', NProgress.done)
@@ -36,15 +36,15 @@ class NextApp extends App {
       }
     }
 
-    return { pageProps, path: ctx.asPath }
+    return { pageProps }
   }
 
   render() {
-    const { Component, pageProps, path } = this.props
+    const { Component, pageProps } = this.props
 
     return (
       <ErrorBoundary FallbackComponent={Error}>
-        <DefaultHeadTags />
+        <HashiHead is={Head} title="Nomad by HashiCorp" />
         <MegaNav product="Nomad" />
         <ProductSubnav />
         <Component {...pageProps} />
--- website/pages/_document.js
+++ website/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import HashiHead from '@hashicorp/react-head'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,10 +10,32 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" type="text/css" href="/css/nprogress.css" />
-        </Head>
+        <HashiHead
+          is={Head}
+          siteName="Nomad by HashiCorp"
+          description="Nomad is a highly available, distributed, data-center aware cluster and application scheduler designed to support the modern datacenter with support for long-running services, batch jobs, and much more."
+          image="https://www.nomadproject.io/img/og-image.png"
+          stylesheet={[
+            { href: '/css/nprogress.css' },
+            {
+              href:
+                'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap'
+            }
+          ]}
+          icon={[{ href: '/favicon.ico' }]}
+          preload={[
+            { href: '/fonts/klavika/medium.woff2', as: 'font' },
+            { href: '/fonts/gilmer/light.woff2', as: 'font' },
+            { href: '/fonts/gilmer/regular.woff2', as: 'font' },
+            { href: '/fonts/gilmer/medium.woff2', as: 'font' },
+            { href: '/fonts/gilmer/bold.woff2', as: 'font' },
+            { href: '/fonts/metro-sans/book.woff2', as: 'font' },
+            { href: '/fonts/metro-sans/regular.woff2', as: 'font' },
+            { href: '/fonts/metro-sans/semi-bold.woff2', as: 'font' },
+            { href: '/fonts/metro-sans/bold.woff2', as: 'font' },
+            { href: '/fonts/dejavu/mono.woff2', as: 'font' }
+          ]}
+        />
         <body>
           <Main />
           <NextScript />
